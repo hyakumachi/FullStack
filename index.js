b80c21941682d146f1b24aaa8275779f1a75936b
Bernard Louie <94907313+hyakumachi@users.noreply.github.com>
2025-08-07 13:52:27 +0800

    fix: fix typo in server (req, res)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 
-const server = http.createServer((res,res)=>{
+const server = http.createServer((req, res)=>{
     res.setHeader('Content-Type','text/html')
 
     let myUrl = './views/'
